734a6275f8f5ac209ad0c5365e663f210c0e7839
litthy <litthy@qq.com>
2025-01-08 16:49:35 +0800

    feat: 2023中秋

---
+++ src/components/card/Card2024Spring.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="card2024Spring" @click.stop>
+        <div class="u-springFestival-card fadeInUpBig p-animation">
+            <div class="u-letter u-img m-animation" ref="letter" @click="close">
+                <img class="u-img" :src="letter" alt="新年快乐" />
+                <img class="u-img" :src="countImg" alt="新年快乐" />
+            </div>
+            <img class="u-cover u-img" ref="cover" :src="cover" alt="新年快乐" @click="open" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "card2024Spring",
+    props: ["data"],
+    computed: {
+        cover() {
+            return this.data.cover;
+        },
+        letter() {
+            return this.data.letter;
+        },
+        countImg() {
+            return this.data.countImg;
+        },
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        open() {
+            const letter = this.$refs.letter;
+            const cover = this.$refs.cover;
+            cover.classList.add("click");
+            letter.classList.add("fadeOutDownBig");
+            letter.addEventListener(
+                "animationend",
+                () => {
+                    letter.classList.remove("fadeOutDownBig");
+                    letter.classList.add("fadeInDownBig");
+                },
+                { once: true }
+            );
+        },
+        close() {
+            this.$emit("close");
+        },
+    },
+};
+</script>
+<style lang="less">
+@import "~@/assets/css/animation.less";
+.m-animation {
+    -webkit-animation-duration: 0.5s;
+    animation-duration: 0.5s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+}
+.card2024Spring {
+    .pf;
+    .full;
+    .lt(0);
+    .u-springFestival-card {
+        .pointer;
+        .size(392px,720px);
+        .ct(o,392px,720px);
+        .u-img {
+            .pa;
+            .full;
+            .lt(0);
+            transition: all 0.5s;
+        }
+        &:hover {
+            .u-letter {
+                top: 5%;
+            }
+        }
+        .u-cover {
+            transition: all 1.5s;
+            &.click {
+                transform: rotate(5deg);
+            }
+        }
+        .u-letter {
+            .size(calc(100% - 8px));
+            .ml(4px);
+        }
+        .u-letter.fadeInDownBig {
+            .z(1);
+            top: 0 !important;
+        }
+    }
+}
+@media screen and (min-width: @2k) {
+    .card2024Spring {
+        .u-springFestival-card {
+            .size(600px,1100px);
+            .ct(o,600px,1100px);
+        }
+    }
+}
+</style>
--- src/views/HolidayCard.vue
+++ src/views/HolidayCard.vue
@@ -1,32 +1,159 @@
 <template>
     <AppLayout>
-        <div class="m-main"></div>
+        <div class="m-main m-index-popup">
+            <component :is="event_component" :data="component_data" @checked="checked" @close="goBack" />
+        </div>
     </AppLayout>
 </template>
 
 <script>
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import cardType from "@/assets/data/card.json";
 import { getHolidayCard } from "@/service/card";
 import AppLayout from "@/layouts/AppLayout.vue";
+import DefaultTemplate from "@/components/card/DefaultTemplate.vue";
+import Card2024Children from "@/components/card/Card2024Children.vue";
+import Card2024Spring from "@/components/card/Card2024Spring.vue";
+import Card2024NewYear from "@/components/card/Card2024NewYear.vue";
+import Card2023Christmas from "@/components/card/Card2023Christmas.vue";
 export default {
     name: "holidayCard",
-    components: { AppLayout },
-    props: [],
+    components: { AppLayout, DefaultTemplate, Card2024Children, Card2024Spring, Card2024NewYear, Card2023Christmas },
     data: function () {
-        return {};
+        return {
+            cardType,
+            check: false,
+            list: [],
+        };
     },
-    computed: {},
-    watch: {},
-    methods: {
-        load() {
-            getHolidayCard().then((res) => {
-                console.log(res);
-            });
+    computed: {
+        // 用户id
+        user_id() {
+            return this.$route.params.uid;
+        },
+        // 打开的卡号id
+        my_card_id() {
+            return this.$route.params.id;
+        },
+        // 当前活动的信息
+        active_event() {
+            return this.list.find((item) => item.id == this.my_card_id);
+        },
+        // 当前卡号对应的活动id
+        event_id() {
+            return this.active_event?.event_id;
+        },
+        // 活动id对应的活动key
+        event_key() {
+            return this.cardType[this.event_id]?.key;
+        },
+        // 活动对应的图片地址
+        imgLink() {
+            return __imgPath + `topic/festival/${this.event_key}/`;
+        },
+        // 活动对应的图片列表
+        imgList() {
+            return this.cardType[this.event_id]?.imgList || [];
+        },
+        // 活动抽中的数量
+        fontCount() {
+            return this.active_event?.event_reward_count;
+        },
+        // 当前组件
+        event_component() {
+            return this.cardType[this.event_id]?.component;
+        },
+        // 组件数据
+        component_data() {
+            const _data = {
+                default: this.default_data,
+                children: this.children_data,
+                spring: this.spring_data,
+                newyear: this.newyear_data,
+                christmas: this.christmas_data,
+            };
+            return _data[this.cardType[this.event_id]?.type];
+        },
+        // 默认数据
+        default_data() {
+            const bg = this.cardType[this.event_id]?.bg;
+            const count = this.cardType[this.event_id]?.count;
+            const countImgList = String(this.fontCount)
+                .split("")
+                .map((item) => `${this.imgLink}${item}.png`);
+
+            const data = {
+                imgList: this.imgList.map((item) => `${this.imgLink}${item}`),
+                fontCount: this.fontCount,
+                countImg: `${this.imgLink}${this.fontCount}.png`,
+                countImgList,
+                count,
+            };
+            if (bg) data.countBg = `${this.imgLink}${bg}`;
+            return data;
+        },
+        // 儿童节
+        children_data() {
+            return {
+                bgImg: `${this.imgLink}bg.png`,
+                buttonImg: `${this.imgLink}button.png`,
+                bg2Img: `${this.imgLink}bg2.png`,
+                countImg: `${this.imgLink}${this.fontCount}.png`,
+            };
+        },
+        // 春节
+        spring_data() {
+            return {
+                letter: `${this.imgLink}letter.png`,
+                cover: `${this.imgLink}cover.png`,
+                countImg: `${this.imgLink}${this.fontCount}.png`,
+            };
+        },
+        // 元旦
+        newyear_data() {
+            return {
+                fontCount: this.fontCount,
+                imgList: this.imgList.map((item) => `${this.imgLink}${item}`),
+            };
+        },
+        // 圣诞
+        christmas_data() {
+            return {
+                countImg: `${this.imgLink}${this.fontCount}.png`,
+                imgList: this.imgList.map((item) => `${this.imgLink}${item}`),
+            };
         },
     },
-    mounted () {
-        this.load();
-    }
+    watch: {
+        my_card_id: {
+            handler: function (id) {
+                id ? this.load() : this.goBack();
+            },
+            immediate: true,
+        },
+    },
+    methods: {
+        goBack() {
+            this.$router.push({ name: "index", params: { id: this.user_id } });
+        },
+        load() {
+            getHolidayCard().then((res) => {
+                this.list = res?.data || [];
+                // const ids = this.list.map((item) => item.id);
+                // if (!ids.includes(this.my_card_id)) this.goBack();
+            });
+        },
+        checked() {
+            this.check = true;
+        },
+    },
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less">
+.m-main {
+    height: 100vh;
+    background-color: #24292e;
+}
+@import "~@/assets/css/pop.less";
+</style>
