6d33c9aa7547f11fb837c25ffabd80892cef79b5
litthy <litthy@qq.com>
2025-02-19 13:05:18 +0800

    feat: card

--- src/components/birthday/default2025.vue
+++ src/components/birthday/default2025.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="p-birthday">
+    <div class="p-birthday" @mousemove="handleMouseMove" @mouseleave="handleMouseLeave">
         <video class="u-video" :src="`${imgPath}bg.mp4`" type="video/mp4" autoplay loop muted></video>
-        <div :class="`m-letter ${open ? 'open' : ''}`" @click="openLetter">
+        <div :class="letterClass" @click="openLetter">
             <!-- 内页背景 -->
             <img :src="`${imgPath}pack_in.png`" class="u-img u-pack-in" />
             <!-- 外信封下部 -->
@@ -10,17 +10,21 @@
             <img :src="`${imgPath}pack_topout.png`" class="u-img u-pack-top-out" />
             <img :src="`${imgPath}pack_topin.png`" class="u-img u-pack-top-in" />
             <!-- 信封内容 -->
-            <img :src="`${imgPath}${star}/girl.png`" class="u-img u-girl" />
-            <div class="m-paper">
-                <img :src="`${imgPath}paper.png`" class="u-bg" />
-                <span class="u-text name">{{ name }}</span>
-                <span class="u-text date">{{ date }}</span>
+            <div class="m-content" :style="transformStyle">
+                <img :src="`${imgPath}${star}/girl.png`" class="u-img u-girl" />
+                <div class="m-paper">
+                    <img :src="`${imgPath}paper.png`" class="u-bg" />
+                    <span class="u-text name">{{ name }}</span>
+                    <span class="u-text date">{{ date }}</span>
+                </div>
+                <img :src="`${imgPath}${star}/icon.png`" class="u-img u-icon" />
+                <img :src="`${imgPath}girlQ.png`" class="u-img u-cake" />
+                <img :src="`${imgPath}ground.png`" class="u-img u-ground" />
             </div>
-            <img :src="`${imgPath}${star}/icon.png`" class="u-img u-icon" />
-            <img :src="`${imgPath}${star}/img.png`" class="u-img u-star" />
             <img :src="`${imgPath}${star}/txt.png`" class="u-img u-txt" />
+            <img :src="`${imgPath}${star}/img.png`" class="u-img u-star" />
             <!-- 漆封 -->
-            <div :class="`u-paint ${start ? 'animation' : ''}`">
+            <div :class="`u-paint ${clickCount == 1 ? 'animation' : ''}`">
                 <img :src="`${imgPath}paint.png`" class="u-pack-paint" />
                 <img :src="`${imgPath}ok.png`" class="u-pack-ok" />
             </div>
@@ -39,10 +43,16 @@ export default {
         return {
             data: "",
             imgPath: __cdn + "design/card/birthday/default2025/",
+            star: "SY",
+            // 状态
             start: false,
             open: false,
             diffuse: false,
-            star: "SY",
+            clickCount: 0,
+            // 鼠标位置
+            mouseX: 0,
+            mouseY: 0,
+            isMouseOver: false,
         };
     },
     computed: {
@@ -64,16 +74,24 @@ export default {
         date() {
             return this.dateFormat(this.data.created_at);
         },
-    },
-    watch: {
-        start: function (val) {
-            if (val) {
-                setTimeout(() => {
-                    this.open = true;
-                }, 1000);
-            }
+        letterClass() {
+            return {
+                "m-letter": true,
+                open: this.open,
+                diffuse: this.diffuse,
+            };
+        },
+        transformStyle() {
+            if (this.isMouseOver)
+                return {
+                    transform: `perspective(2000px) translate3d(${this.mouseX}px,${this.mouseY}px, 0px) rotateY(${this.mouseX}deg) rotateX(${this.mouseY}deg)`,
+                };
+            return {
+                transform: `perspective(2000px) translate(${this.mouseX}px,${this.mouseY}px, 0px) rotateY(${this.mouseY}deg) rotateX(${this.mouseX}deg)`,
+            };
         },
     },
+
     mounted() {
         this.loadData();
     },
@@ -88,10 +106,25 @@ export default {
             });
         },
         openLetter() {
-            this.start = true;
-            if (this.start) {
+            this.clickCount++;
+            if (this.clickCount == 1) {
+                setTimeout(() => {
+                    this.open = true;
+                }, 500);
+            }
+            if (this.clickCount == 2) {
                 this.diffuse = true;
             }
+        },
+        handleMouseMove(event) {
+            if (this.clickCount < 2) return;
+            this.mouseX = event.clientX / 100;
+            this.mouseY = event.clientY / 100;
+            this.isMouseOver = true;
+        },
+        handleMouseLeave() {
+            if (this.clickCount < 2) return;
+            this.isMouseOver = false;
         },
     },
 };
